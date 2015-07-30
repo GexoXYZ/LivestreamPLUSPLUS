bf08a2aa4f350ad50b34e899cc0ee33c7f3feeac
Manuel Kraus <me@gexo.xyz>
2015-07-30 14:36:51 +0200

    Initial release

---
+++ Livestream++/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Livestream__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.btn_startStream = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.txt_Channel = new MetroFramework.Controls.MetroTextBox();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            this.metroStyleManager1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Location = new System.Drawing.Point(23, 63);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(254, 502);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabControl1.UseSelectable = true;
+            this.metroTabControl1.SelectedIndexChanged += new System.EventHandler(this.metroTabControl1_SelectedIndexChanged);
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroComboBox1);
+            this.metroTabPage1.Controls.Add(this.btn_startStream);
+            this.metroTabPage1.Controls.Add(this.metroLabel2);
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.Controls.Add(this.txt_Channel);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(246, 460);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Home";
+            this.metroTabPage1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Source (Best)",
+            "High",
+            "Medium",
+            "Low",
+            "Mobile (Worst)",
+            "Audio"});
+            this.metroComboBox1.Location = new System.Drawing.Point(68, 59);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(121, 29);
+            this.metroComboBox1.TabIndex = 8;
+            this.metroComboBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroComboBox1.UseSelectable = true;
+            // 
+            // btn_startStream
+            // 
+            this.btn_startStream.Location = new System.Drawing.Point(3, 110);
+            this.btn_startStream.Name = "btn_startStream";
+            this.btn_startStream.Size = new System.Drawing.Size(82, 23);
+            this.btn_startStream.TabIndex = 9;
+            this.btn_startStream.Text = "Start Stream";
+            this.btn_startStream.UseSelectable = true;
+            this.btn_startStream.Click += new System.EventHandler(this.btn_startStream_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(3, 61);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(53, 19);
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "Quality:";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 22);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(54, 19);
+            this.metroLabel1.TabIndex = 6;
+            this.metroLabel1.Text = "Stream:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // txt_Channel
+            // 
+            this.txt_Channel.Lines = new string[0];
+            this.txt_Channel.Location = new System.Drawing.Point(68, 22);
+            this.txt_Channel.MaxLength = 32767;
+            this.txt_Channel.Name = "txt_Channel";
+            this.txt_Channel.PasswordChar = '\0';
+            this.txt_Channel.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_Channel.SelectedText = "";
+            this.txt_Channel.Size = new System.Drawing.Size(137, 23);
+            this.txt_Channel.TabIndex = 5;
+            this.txt_Channel.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.txt_Channel.UseSelectable = true;
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.webBrowser1);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.HorizontalScrollbarSize = 10;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(246, 460);
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Chat";
+            this.metroTabPage2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            this.metroTabPage2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage2.VerticalScrollbarSize = 10;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(243, 454);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 267);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Livestreamer++";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton btn_startStream;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox txt_Channel;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Livestream++/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+using Newtonsoft.Json;
+
+namespace Livestream__
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            int id = 0;     
+            RegisterHotKey(Handle, id, (int)KeyModifier.Alt, Keys.L.GetHashCode());
+        }
+
+        #region GlobalHotkey
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+ 
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+ 
+
+ 
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+ 
+            if (m.Msg == 0x0312)
+            {
+                /* Note that the three lines below are not needed if you only want to register one hotkey.
+                 * The below lines are useful in case you want to register multiple keys, which you can use a switch with the id as argument, or if you want to know which key/modifier was pressed for some particular reason. */
+ 
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = m.WParam.ToInt32();                                        // The id of the hotkey that was pressed.
+    
+                Show();
+                WindowState = FormWindowState.Normal;
+                
+
+            }
+        }
+        #endregion
+
+        void ExecuteCommand(string channel)
+        {
+            
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " +   "livestreamer twitch.tv/" + channel + " " + quality());
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            
+            process.Close();
+        }
+
+        public string quality()
+        {
+            string selectedItem = "";
+            if (metroComboBox1.InvokeRequired)
+            {
+                metroComboBox1.Invoke(new MethodInvoker(delegate { selectedItem = metroComboBox1.SelectedItem.ToString(); }));
+            }
+           
+
+            if (isUserPartner(txt_Channel.Text))
+            {
+                if (selectedItem == "Source (Best)")
+                {
+                    return "source";
+                }
+                if (selectedItem == "High")
+                {
+                    return "high";
+                }
+                if (selectedItem == "Medium")
+                {
+                    return "medium";
+                }
+                if (selectedItem == "Low")
+                {
+                    return "low";
+                }
+                if (selectedItem == "Mobile (Worst)")
+                {
+                    return "mobile";
+                }
+                return "audio";
+            }
+            if (selectedItem !="Source (Best)")
+            {
+
+
+                MetroMessageBox.Show(this,
+                    "Forced source quality because " + txt_Channel.Text + " doesn't have quality options!",
+                    "Quality = Source", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                metroComboBox1.Text = "Source (Best)";
+
+                return "source";
+            }
+            return "penis";
+
+
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(Handle, 0);    
+            Application.Exit();
+        }
+
+        private bool isUserLive(string username)
+        {
+            string sUsername = username;
+
+            string sUrl = "https://api.twitch.tv/kraken/streams/" + sUsername;
+            HttpWebRequest wRequest = (HttpWebRequest) HttpWebRequest.Create(sUrl);
+            wRequest.ContentType = "application/json";
+            wRequest.Accept = "application/vnd.twitchtv.v3+json";
+            wRequest.Method = "GET";
+
+            dynamic wResponse = wRequest.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(wResponse);
+            dynamic res = reader.ReadToEnd();
+            reader.Close();
+            wResponse.Close();
+
+
+            try
+            {
+                string live = ((dynamic) JsonConvert.DeserializeObject(res)).stream._id;
+                return true;
+            }
+            catch
+            {
+                
+            }
+            return false;
+        }
+        private bool isUserPartner(string username)
+        {
+            string sUsername = username;
+
+            string sUrl = "https://api.twitch.tv/kraken/streams/" + sUsername;
+            HttpWebRequest wRequest = (HttpWebRequest)HttpWebRequest.Create(sUrl);
+            wRequest.ContentType = "application/json";
+            wRequest.Accept = "application/vnd.twitchtv.v3+json";
+            wRequest.Method = "GET";
+
+            dynamic wResponse = wRequest.GetResponse().GetResponseStream();
+            StreamReader reader = new StreamReader(wResponse);
+            dynamic res = reader.ReadToEnd();
+            reader.Close();
+            wResponse.Close();
+
+            string live = ((dynamic)JsonConvert.DeserializeObject(res)).stream.channel.partner;
+            if (live == "True")
+                return true;
+            return false;
+        }
+
+
+
+    
+
+        private void btn_startStream_Click(object sender, EventArgs e)
+        {
+            if (txt_Channel.Text == "" || metroComboBox1.SelectedItem == null)
+            {
+                MetroFramework.MetroMessageBox.Show(this,
+                    "Please determine a proper username and/or select a Stream quality.","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    if (isUserLive(txt_Channel.Text))
+                    {
+                        webBrowser1.Navigate("http://twitch.tv/" + txt_Channel.Text + "/chat?popout=");
+                        
+
+                        //Hide();
+                        BackgroundWorker bw = new BackgroundWorker();
+
+                        bw.WorkerReportsProgress = true;
+                        bw.DoWork += new DoWorkEventHandler(
+                            delegate(object o, DoWorkEventArgs args)
+                            {
+                                BackgroundWorker b = o as BackgroundWorker;
+                                ExecuteCommand(txt_Channel.Text.ToLower());
+                            });
+                        bw.RunWorkerAsync();
+                        
+                    }
+                    else
+                    {
+                        MetroFramework.MetroMessageBox.Show(this,
+                       txt_Channel.Text + " does not seem to be streaming right now!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception)
+                {
+                    MetroFramework.MetroMessageBox.Show(this,
+                       txt_Channel.Text + " was  not found! Please make sure you didn't misspell the name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+                
+                
+                
+                
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void metroTabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (metroTabControl1.SelectedIndex == 1)
+            {
+                Size = new Size(350, 588);
+                metroTabControl1.Size = new Size(304, 502);
+                webBrowser1.Size = new Size(300, 454);
+            }
+            else if (metroTabControl1.SelectedIndex == 0)
+            {
+                Size = new Size(300, 267);
+                metroTabControl1.Size = new Size(254, 502);
+            }
+        }
+
+
+
+
+
+       
+
+
+    }
+}
